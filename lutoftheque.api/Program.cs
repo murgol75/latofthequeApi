289e12cb87de429329136bb1c49381e53fa7a0b2
murgol75 <murgol@yahooo.fr>
2023-12-22 00:56:32 +0100

    entity framework installé + sqlconnection

---
+++ lutoftheque.api/Program.cs
@@ -0,0 +1,32 @@
+using lutoftheque.Entity.Models;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var lutofthequeConnectionString = builder.Configuration.GetConnectionString("lutofthequeConnection");
+
+
+builder.Services.AddDbContext<lutofthequeContext>(opts => opts.UseSqlServer(lutofthequeConnectionString));
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
