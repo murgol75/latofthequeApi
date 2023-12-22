cfbea5d7ff1bddede0f6b747cc247248d65c6b0a
murgol75 <murgol@yahooo.fr>
2023-12-23 00:05:51 +0100

    creation du service

---
+++ lutoftheque.api/Services/GameService.cs
@@ -0,0 +1,18 @@
+﻿using lutoftheque.Entity.Models;
+
+namespace lutoftheque.api.Services
+{
+    public class GameService
+    {
+        private readonly lutofthequeContext _lutofthequeContext;
+        public GameService (lutofthequeContext _lutofthequeContext)
+        {
+            this._lutofthequeContext = _lutofthequeContext;
+        }
+        public List<Game> GetGames ()
+        {
+            return _lutofthequeContext.Games.ToList (); // correspond à "select * from Games"
+        }
+
+    }
+}
